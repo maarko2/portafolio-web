dddd21b2c5cd05a4bd37d59760573ed21761dc57
maarko <maarko.burgos@gmail.com>
2025-07-14 23:04:36 -0400

    Actualización de imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://marcoburgos.me',
+  base: '/',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
